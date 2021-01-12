dad1560a7cffdf3f0c750bf2caec265f57a09494
JJ <fidojj@hotmail.com>
2021-01-13 08:44:30 +1100

    Fixed hello.asm example to work with new syscall ABI

--- Base/hello.asm
+++ Base/hello.asm
@@ -1,14 +1,16 @@
+global _start
+
 _start:
     mov rax, 4 ; Write
     
-    mov rbx, 0 ; Stdout
-    mov rcx, string ; Data
+    mov rdi, 0 ; Stdout
+    mov rsi, string ; Data
     mov rdx, string_end - string ; Length
     int 0x69 ; Write
     
     mov rax, 1 ; Exit
     
-    mov rbx, 0
+    mov rdi, 0
     int 0x69 ; Exit
 string:
  db "Hello, Lemon OS World!"
