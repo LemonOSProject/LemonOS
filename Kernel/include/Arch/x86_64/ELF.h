d38d664da7aa37933d5ca6c85b87eb19791eef97
JJ <fidojj@hotmail.com>
2021-04-07 11:06:38 +1000

    Kernel: Extend ELF.h definitions, remove -fPIC when building modules

--- Kernel/include/Arch/x86_64/ELF.h
+++ Kernel/include/Arch/x86_64/ELF.h
@@ -60,6 +60,18 @@ typedef struct ELF64Section {
 	uint64_t entSize; // Size of entries if the section is a table
 } __attribute__((packed)) elf64_section_t;
 
+typedef struct ELF64Relocation {
+	uint64_t offset; // Address of reference
+	uint64_t info; // Symbol index and relocation type
+} __attribute__((packed)) elf64_rel_t;
+
+
+typedef struct ELF64RelocationA {
+	uint64_t offset; // Address of reference
+	uint64_t info; // Symbol index and relocation type
+	int64_t addend; // Constant addend
+} __attribute__((packed)) elf64_rela_t;
+
 typedef struct {
 	uint64_t entry;
 	uint64_t pHdrSegment;
@@ -68,6 +80,10 @@ typedef struct {
 
 	char* linkerPath;
 } elf_info_t;
+
+#define ELF64_R_SYM(i) ((i) >> 32) // Relocation info to symbol table index
+#define ELF64_R_TYPE(i) ((i) & 0xffffffffL) // Relocation info to relocation type
+#define ELF64_R_INFO(s, t) (((s) << 32) + ((t) & 0xffffffffL)) // Create relocation info value out of index s and type t
 
 #define DT_NULL 0
 #define DT_NEEDED 1
@@ -118,21 +134,48 @@ typedef struct {
 #define SHT_DYNAMIC 6 // Dyanmic linking tables
 #define SHT_NOTE 7 // Note
 #define SHT_NOBITS 8 // Uninitialized 
+#define SHT_REL 9 // 'Rel' relocation entries
 #define SHT_DYNSYM 11 // Dynamic loader symbol table
 
 // Section Attributes
-#define SHF_WRITE 0x1
-#define SHF_ALLOC 0x2
+#define SHF_WRITE 0x1 // Writable
+#define SHF_ALLOC 0x2 // Allocated in memory for program
 #define SHF_EXECINSTR 0x4
 
 // .bss: SHT_NOBITS
 // .data, .interp, .rodata, .text: SHT_PROGBITS
+
+// Symbol Types
+#define STT_NOTYPE 0 // No type specified
+#define STT_OBJECT 1 // Data object
+#define STT_FUNC 2 // Function entry point
+#define STT_SECTION 3 // Section symbol
+#define STT_FILE 4 // Source file associated with object file
+
+// Symbol Bindings
+#define STB_LOCAL 0 // Only visible within object file
+#define STB_GLOBAL 1 // Global symbol
+#define STB_WEAK 2 // Global scope, lower precedence than global symbols
+
+#define ELF64_SYM_TYPE(s) ((s) & 0xf) // Symbol info value to symbol type only
+#define ELF64_SYM_BIND(s) (((s) >> 4) & 0xf) // Symbol info value to symbol binding only
+
+#define ELF64_R_SYM(i) ((i) >> 32) // Symbol table index from relocation info field
+#define ELF64_R_TYPE(i) ((i) & 0xffffffffL) // Relocation type from relocation info
+
+// Relocation types
+#define ELF64_R_X86_64_NONE 0
+#define ELF64_R_X86_64_64 1 // symbol + addend
+#define ELF64_R_X86_64_32 10 // symbol (32-bit) + addend
+#define ELF64_R_X86_64_32S 11 // symbol (32-bit, sign extended) + addend
 
 using ELFHeader = ELF64Header;
 using ELFProgramHeader = ELF64ProgramHeader;
 using ELFDynamicEntry = ELF64DynamicEntry;
 using ELFSymbol = ELF64Symbol;
 using ELFSection = ELF64Section;
+using ELFRelocation = ELF64Relocation;
+using ELFRelocationA = ELF64RelocationA;
 
 typedef struct process process_t;
 
