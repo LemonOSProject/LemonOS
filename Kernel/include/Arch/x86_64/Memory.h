ca8424b1ca7cbd9d31753368f9546d60f1bf385b
JJ <fidojj@hotmail.com>
2021-04-15 11:08:50 +1000

    Kernel: Placement new

--- Kernel/include/Arch/x86_64/Memory.h
+++ Kernel/include/Arch/x86_64/Memory.h
@@ -3,8 +3,13 @@
 #include <PhysicalAllocator.h>
 #include <Paging.h>
 #include <Liballoc.h>
+#include <Compiler.h>
 
 typedef struct {
     uint64_t base;
     uint64_t pageCount;
 } mem_region_t;
+
+ALWAYS_INLINE void* operator new(size_t, void* p){
+	return p;
+}
