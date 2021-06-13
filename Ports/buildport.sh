77290453077f6e4da7f48d4a20ce7c50ffdaa9b4
JJ <fidojj@hotmail.com>
2021-06-13 15:57:03 +1000

    Include GnuBoy in Ports

--- Ports/buildport.sh
+++ Ports/buildport.sh
@@ -6,7 +6,6 @@ if [ -z "$LEMON_SYSROOT" ]; then
     export LEMON_SYSROOT=$HOME/.local/share/lemon/sysroot
 
     echo "warning: LEMON_SYSROOT not set. Automatically set to $LEMON_SYSROOT"
-    read
 fi
 
 export CC=lemon-clang
