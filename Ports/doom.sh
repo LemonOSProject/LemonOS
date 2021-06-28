3f016e5b134959b56a3b29085d5b78d215cee09c
JJ <fidojj@hotmail.com>
2021-06-28 11:35:55 +1000

    Fix header installation, automatically download DOOM shareware WAD

--- Ports/doom.sh
+++ Ports/doom.sh
@@ -7,4 +7,7 @@ buildp(){
  	cd $BUILD_DIR
  	make -f Makefile.lemon -j$JOBCOUNT
  	cp doom.lef "$LEMON_SYSROOT/$LEMON_PREFIX/bin"
+
+	mkdir -p "$LEMON_SYSROOT/system/doom"
+	curl -Lo "$LEMON_SYSROOT/system/doom/doom1.wad" "http://distro.ibiblio.org/pub/linux/distributions/slitaz/sources/packages/d/doom1.wad"
 }
