ce1a70d497cef4302eec0a02e64b8bd2190d67dd
JJ <fidojj@hotmail.com>
2021-10-31 12:28:15 +1100

    Scripts: formatdisk.sh fix GRUB EFI option

--- Scripts/formatdisk.sh
+++ Scripts/formatdisk.sh
@@ -29,7 +29,7 @@ cleanup2(){
 trap 'cleanup2' 1
 
 mkdir -p /mnt/LemonEFI/EFI/BOOT
-if [ ! -z USE_GRUB_EFI ]; then
+if [ ! -z "$USE_GRUB_EFI" ]; then
     grub-install --target=x86_64-efi --boot-directory=/mnt/Lemon/lemon/boot --efi-directory=/mnt/LemonEFI "${LOOPBACK_DEVICE}" --removable
 elif [ -e "$HOME/.local/share/lemon/share/limine/BOOTX64.EFI" ]; then
     cp "$HOME/.local/share/lemon/share/limine/BOOTX64.EFI" /mnt/LemonEFI/EFI/BOOT/
