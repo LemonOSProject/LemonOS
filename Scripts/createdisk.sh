c30b1ebe21ca5a8ff4e22f6525c24d6d6a954e93
Frank Enderman <90335574+FrankEnderman@users.noreply.github.com>
2021-11-21 09:29:24 +0530

    add emojis

--- Scripts/createdisk.sh
+++ Scripts/createdisk.sh
@@ -12,5 +12,5 @@ mkdir -p Disks
 dd if=/dev/zero of=Disks/Lemon.img bs=512 count=4194304
 sfdisk Disks/Lemon.img < Scripts/partitions.sfdisk
 
-echo "Formatting disk!"
+echo "Formatting disk! your files will be deleted tho 🙁"
 sudo Scripts/formatdisk.sh
