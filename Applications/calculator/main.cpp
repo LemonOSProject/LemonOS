9bb572fc9285269a79204cdde7aa8227a745ae64
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2021-11-24 12:13:36 +0530

    new calculator app

---
+++ Applications/calculator/main.cpp
@@ -0,0 +1,10 @@
+#include <Lemon/GUI/Window.h>
+
+#include <Lemon/GUI/Messagebox.h>
+#include <Lemon/System/Spawn.h>
+
+Lemon::GUI::Window* window;
+window = new Lemon::GUI::Window("Addition", {600, 348}, WINDOW_FLAGS_RESIZABLE, Lemon::GUI::WindowType::GUI);
+box = new Lemon::GUI::textInput("Input 1")
+box = new Lemon::GUI::textInput("Input 2")
+type RES == Lemon::GUI::textInput("Input 1") + Lemon::GUI::textInput("Input 2") 
