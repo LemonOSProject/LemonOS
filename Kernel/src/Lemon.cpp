6f4fb649cd2cc05ffaad8f4f5934936c991b3d0a
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2021-11-27 20:59:04 +0530

    update lemon version string

--- Kernel/src/Lemon.cpp
+++ Kernel/src/Lemon.cpp
@@ -1,3 +1,3 @@
 namespace Lemon {
-char* versionString = "Lemon OS Nightly (Alpha) x86_64, " __DATE__ " " __TIME__;
+char* versionString = "ButterOS 2021 (Internal Testing/preview 1) x86_64, " __DATE__ " " __TIME__;
 }
