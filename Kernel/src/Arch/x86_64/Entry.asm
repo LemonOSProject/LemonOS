b1f8672b15145a79719bf4b037198f560147b34f
fido2020 <computerfido@gmail.com>
2022-08-23 18:10:04 +1000

    Make sure code and data segments are in the right place for the syscall instruction
    This should've been pushed with the previous commit 💀

--- Kernel/src/Arch/x86_64/Entry.asm
+++ Kernel/src/Arch/x86_64/Entry.asm
@@ -64,19 +64,19 @@ GDT64:                           ; Global Descriptor Table (64-bit).
     db 10010010b                 ; Access (read/write).
     db 00000000b                 ; Granularity.
     db 0                         ; Base (high).
-    .UserCode: equ $ - GDT64     ; The usermode code descriptor.
-    dw 0                         ; Limit (low).
-    dw 0                         ; Base (low).
-    db 0                         ; Base (middle)
-    db 11111010b                 ; Access (exec/read).
-    db 00100000b                 ; Granularity, 64 bits flag, limit19:16.
-    db 0                         ; Base (high).
     .UserData: equ $ - GDT64     ; The usermode data descriptor.
     dw 0                         ; Limit (low).
     dw 0                         ; Base (low).
     db 0                         ; Base (middle)
     db 11110010b                 ; Access (read/write).
     db 00000000b                 ; Granularity.
+    db 0                         ; Base (high).
+    .UserCode: equ $ - GDT64     ; The usermode code descriptor.
+    dw 0                         ; Limit (low).
+    dw 0                         ; Base (low).
+    db 0                         ; Base (middle)
+    db 11111010b                 ; Access (exec/read).
+    db 00100000b                 ; Granularity, 64 bits flag, limit19:16.
     db 0                         ; Base (high).
     .TSS: ;equ $ - GDT64         ; TSS Descriptor
     .len:
