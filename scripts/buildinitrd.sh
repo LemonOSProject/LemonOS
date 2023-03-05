cb3ee097cf777da344160bb01c63f3b0c919c7d6
fido2020 <computerfido@gmail.com>
2023-03-05 15:59:11 +1100

    bootstrap.yml, scripts: Fix paths and dependencies

--- scripts/buildinitrd.sh
+++ scripts/buildinitrd.sh
@@ -16,7 +16,7 @@ mkdir -p $INITRDDIR/modules
 cp -ru kernel/resources/* $INITRDDIR
 cp -L build//sysroot/system/lib/libc.so* build//sysroot/system/lib/libc++*.so* build//sysroot/system/lib/libunwind.so* build//sysroot/system/lib/ld.so* build//sysroot/system/lib/libfreetype.so* build//sysroot/system/lib/libpthread.so* build//sysroot/system/lib/librt.so* build//sysroot/system/lib/libdl.so* $INITRDDIR # Only copy crucial libraries
 cp build/sysroot/system/bin/lsh $INITRDDIR # Create a backup of LSh on the ramdisk for FTerm
-cp build/packages/lemon-utils/system/bin/* $INITRDDIR # Create a backup of LemonUtils on the ramdisk for FTerm
+cp $(find build/pkg-builds/lemon-system/utilities -maxdepth 1 -type f -not -name '*.*') $INITRDDIR # Create a backup of LemonUtils on the ramdisk for FTerm
 
 cp -r build/pkg-builds/lemon-kernel/modules/*.sys $INITRDDIR/modules # Add kernel modules
 
--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e 1
+
 docker pull computerfido/lemontoolchain:latest
 
 SPATH=$(dirname $(readlink -f "$0"))
@@ -7,13 +9,14 @@ SPATH=$(dirname $(readlink -f "$0"))
 cd $SPATH/..
 
 mkdir -p Build
+ln -sfT Build build
+ln -sfT Toolchain toolchain
+ln -sfT ports Ports
 
 cp scripts/docker-bootstrap-site.yml build/bootstrap-site.yml
 echo "    create_extra_args: ['--user', '$(id -u):$(id -g)']" >> build/bootstrap-site.yml
 
-set -e 1
-
-cd Build/
+cd build/
 xbstrap init ..
 docker run -v "$SPATH/..:/var/LemonOS" -w $(pwd) --user $(id -u):$(id -g) -it computerfido/lemontoolchain:latest sh -c "zstd -d /var/lemon-tools.tar.zst --stdout | tar -oxvf - -C /var/LemonOS --strip-components=1; chown -hR $(id -u) /var/LemonOS"
 
